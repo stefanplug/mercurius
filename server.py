f7df3640ea3d66446a8d867fc42ea789e18f72a5
Stefan <stefan@stefan.(none)>
2013-04-25 15:51:46 +0200

    trivial

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from scapy.all import *
 def main(argv):
 	while 1:
 		recieved = sniff(filter="tcp and port 80", count=1)
-		print ord(recieved[0].sport - 10000)
+		print chr(recieved[0].sport - 10000)
 
 if __name__ == '__main__':
 	main(sys.argv)
