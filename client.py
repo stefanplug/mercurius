76db4885a16a1b789103e57909396d49b0503772
Stefan <stefan@stefan.(none)>
2013-04-25 15:50:37 +0200

    trivial

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import sys
 from scapy.all import *
 
 def main(argv):
-	packet = IP(dst='192.168.1.2')
+	packet = IP(dst='192.168.10.2')
 	segment = TCP(dport=80, flags=0x02)
 	
 	file = 'mrbaasman : P@s5w0Rt'
