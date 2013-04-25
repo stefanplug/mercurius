030c6ca3143ad71274b73afa0ef9f127f5104ec4
Stefan <stefan@stefan.(none)>
2013-04-25 14:28:58 +0200

    trivial

--- client.py
+++ client.py
@@ -21,8 +21,8 @@ def create_packet():
 	#packet.ttl =
 	#packet.proto =
 	#packet.chksum =
-	#packet.src = '192.168.10.100'
-	packet.dst = '91.211.73.6'
+	packet.src = '192.168.10.100'
+	packet.dst = '192.168.10.2'
 	#packet.options =
 	return packet
 
