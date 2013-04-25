4579c642229abd041e51415e70240bb1b3e9bc0b
Stefan <stefan@stefan.(none)>
2013-04-25 17:18:18 +0200

    trivial

--- server.py
+++ server.py
@@ -21,11 +21,11 @@ def main(argv):
 	#clear = decrypt(key, 'qfwgw8gvnx2dzHO4d2H4Mzpw4istBIFvfn2dX/4S/QU=')
 	#print clear
 
-	msg = []
+	msg = ''
 	while 1:
 		recieved = sniff(filter="tcp and port 80", count=1)
 		msg.append(chr(recieved[0].sport - 10000))
-		print str(msg)
+		print msg
 
 if __name__ == '__main__':
 	main(sys.argv)
