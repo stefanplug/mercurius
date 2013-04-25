3f9557de8f9b625b1113ff39d1b9ad4a6778832d
Stefan <stefan@stefan.(none)>
2013-04-25 14:40:51 +0200

    trivial

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ import sys
 from scapy.all import *
 
 def main(argv):
-	recieved = sniff(filter="tcp and port 80", count=1, prn=lambda x:x.sprintf("{IP: From %IP.src%: %TCP.dport%}"))
+	for 1:
+		recieved = sniff(filter="tcp and port 80", count=1) #prn=lambda x:x.sprintf("{TCP src: %TCP.sport%}"))
+		print recieved[0]
 
 if __name__ == '__main__':
 	main(sys.argv)
