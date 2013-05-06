1c67c1d29c0dffa359af68fc683056cdf37c80bd
Stefan <stefan@stefan.(none)>
2013-05-06 11:54:17 +0200

    Sup yo

--- server.py
+++ server.py
@@ -16,10 +16,10 @@ def decrypt(key, msg):
 	return clear
 	
 def main(argv):
-	#key = ':Yjds52%9wnsjp>)'
+	key = ':Yjds52%9wnsjp>)'
 	#msg = 'mrbaasman : P@s5w0Rt'
-	#clear = decrypt(key, 'qfwgw8gvnx2dzHO4d2H4Mzpw4istBIFvfn2dX/4S/QU=')
-	#print clear
+	clear = decrypt(key, 'qfwgw8gvnx2dzHO4d2H4Mzpw4istBIFvfn2dX/4S/QU=')
+	print clear
 
 	msg = ''
 	while 1:
